3f0a5e7e332afda024f1cdbdcd0325b83377a8da
ahotorino <edoardo.fantolino@gmail.com>
2026-06-13 14:56:54 +0200

    adding supabase

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from supabase import create_client
+
+
+url = "https://yinthengfapdhtvgidoi.supabase.co"
+key = "sb_publishable_P4Py0xWkBg2YAU5Wm0fpcw_Nj0--Ju9"
+supabase = create_client(url, key)
+
+response = supabase.table("transactions").select("*").execute()
+data = response.data
+df = pd.DataFrame(data)
 
 # from utils.loader import load_data
 
@@ -10,5 +20,7 @@ st.set_page_config(
 )
 
 st.title("Transaction Dashboard")
+
+st.metric("Numero Transazioni", len(df))
 
 # df = load_data()
